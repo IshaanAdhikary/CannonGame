5f3aa180efbea69a51796cf361a5295ed8bca88c
Low-SkillCoder <IshaanAdhikary@outlook.com>
2021-12-09 09:15:38 -0500

    Started Adding ChainBuilder Script

---
+++ 2DCannonPlatformer/Assets/Editor/ChainBuilderEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ChainBuilderScript))]
+public class ChainBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ChainBuilderScript myScript = (ChainBuilderScript)target;
+        if (GUILayout.Button("Build Chain"))
+        {
+            myScript.BuildChain();
+        }
+    }
+}
---
+++ 2DCannonPlatformer/Assets/Scripts/ChainBuilderScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainBuilderScript : MonoBehaviour
+{
+    public GameObject chainHub;
+    public GameObject chainType1;
+    public GameObject chainType2;
+    public int chainLength;
+
+    public void BuildChain()
+    {
+        GameObject cap = Instantiate(chainHub, transform.position, Quaternion.identity);
+        cap.transform.parent = gameObject.transform;
+
+        for (int i = 0; i < chainLength; i++)
+        {
+            float chain1Pos = transform.position.y - (0.25f + (0.4f * i-1));
+
+            Instantiate(chainType1, transform.position, Quaternion.identity);
+            Instantiate(chainType2, transform.position, Quaternion.identity);
+        }
+    }
+}
