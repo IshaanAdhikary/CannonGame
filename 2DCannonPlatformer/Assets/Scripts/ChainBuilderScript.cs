3e9f2e141ec50a8d79982ef2d9a9762d351579bd
Low-SkillCoder <IshaanAdhikary@outlook.com>
2021-12-13 14:20:30 -0500

    Fixed Friction and ChainGen almost done

--- 2DCannonPlatformer/Assets/Scripts/ChainBuilderScript.cs
+++ 2DCannonPlatformer/Assets/Scripts/ChainBuilderScript.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class ChainBuilderScript : MonoBehaviour
 {
+    public enum ChainEnd { Nothing, Mace }
     public GameObject chainHub;
     public GameObject chainType1;
     public GameObject chainType2;
     public int chainLength;
 
+    private ChainEnd ending;
+    private Rigidbody2D prevRB;
+
     public void BuildChain()
     {
+        ending = ChainEnd.Nothing;
         GameObject cap = Instantiate(chainHub, transform.position, Quaternion.identity);
         cap.transform.parent = gameObject.transform;
+        prevRB = cap.GetComponent<Rigidbody2D>();
 
         for (int i = 0; i < chainLength; i++)
         {
-            float chain1Pos = transform.position.y - (0.25f + (0.4f * i-1));
+            Vector3 chain1Pos = transform.position + new Vector3(0, -0.8f * i - 0.25f, 0);
+            Vector3 chain2Pos = transform.position + new Vector3(0, -0.8f * i - 0.65f, 0);
 
-            Instantiate(chainType1, transform.position, Quaternion.identity);
-            Instantiate(chainType2, transform.position, Quaternion.identity);
+            GameObject chain1 = Instantiate(chainType1, chain1Pos, Quaternion.identity);
+            GameObject chain2 = Instantiate(chainType2, chain2Pos, Quaternion.identity);
+            chain1.transform.parent = gameObject.transform;
+            chain2.transform.parent = gameObject.transform;
+
+            HingeJoint2D hinge1 = chain1.GetComponent<HingeJoint2D>();
+            HingeJoint2D hinge2 = chain2.GetComponent<HingeJoint2D>();
+            hinge1.connectedBody = prevRB;
+            hinge2.connectedBody = hinge1.attachedRigidbody;
+            prevRB = hinge2.attachedRigidbody;
+        }
+
+        switch (ending)
+        {
+            case ChainEnd.Mace:
+                Debug.Log("Mace");
+                break;
         }
     }
 }
